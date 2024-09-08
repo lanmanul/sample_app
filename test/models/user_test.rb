ba1c6f33dd138a550fe17f5c2e967e88589fd9fe
lanmanul <gatecrash3r@gmail.com>
2024-09-08 13:12:48 +0300

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,4 +62,9 @@ first.last@foo.jp alice+bob@baz.cn]
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
