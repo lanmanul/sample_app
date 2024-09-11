6bac8061d9427858901eecb1a3465736bde92e8b
lanmanul <gatecrash3r@gmail.com>
2024-09-11 18:17:27 +0300

    Add user microposts

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require 'mini_magick'
+MiniMagick.cli = :imagemagick
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +20,6 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.active_storage.analyzers.append ActiveStorage::Analyzer::ImageAnalyzer::MiniMagick
   end
 end
