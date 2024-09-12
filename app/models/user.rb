8e3fd427e5719509a5823ba4aa40acfba5e98b47
lanmanul <gatecrash3r@gmail.com>
2024-09-12 22:56:00 +0300

    erb to slim

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,7 +76,6 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-
   # Defines a proto-feed.
   # See "Following users" for the full implementation.
   def feed
@@ -85,7 +84,6 @@ WHERE follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids})
 OR user_id = :user_id", user_id: id)
   end
-
 
   # Follows a user.
   def follow(other_user)
